023b23cdfd028918cb4567099cb039ed75e75edb
Mezei Nicoleta <nicoletta.app@gmail.com>
2020-02-13 01:34:02 +0000

    1st code test

--- gymApp_03/Form1.Designer.cs
+++ gymApp_03/Form1.Designer.cs
@@ -37,6 +37,14 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1_ListaNume = new System.Windows.Forms.ComboBox();
+            this.comboBox2_Cartele = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,44 +53,46 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(21, 365);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.Size = new System.Drawing.Size(119, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Cod Abonat";
+            this.label1.Text = "Nume Prenume";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(21, 436);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(127, 20);
+            this.label2.Size = new System.Drawing.Size(141, 20);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Nume / Prenume";
+            this.label2.Text = "Cod Cartela Acces";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(829, 278);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.Size = new System.Drawing.Size(92, 20);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Text = "Abonament";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(79, 278);
+            this.label4.Location = new System.Drawing.Point(21, 512);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 20);
+            this.label4.Size = new System.Drawing.Size(141, 20);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Foto ";
+            this.label4.Text = "Instructor/Mentiuni";
             // 
             // label_Mesaj
             // 
             this.label_Mesaj.AutoSize = true;
             this.label_Mesaj.Location = new System.Drawing.Point(829, 436);
+            this.label_Mesaj.MaximumSize = new System.Drawing.Size(400, 0);
             this.label_Mesaj.Name = "label_Mesaj";
-            this.label_Mesaj.Size = new System.Drawing.Size(0, 20);
+            this.label_Mesaj.Size = new System.Drawing.Size(130, 20);
             this.label_Mesaj.TabIndex = 4;
+            this.label_Mesaj.Text = "mesaj confirmare";
             // 
             // button1
             // 
@@ -104,11 +114,99 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(25, 57);
+            this.pictureBox1.Location = new System.Drawing.Point(37, 39);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(159, 205);
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1_ListaNume
+            // 
+            this.comboBox1_ListaNume.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1_ListaNume.FormattingEnabled = true;
+            this.comboBox1_ListaNume.Items.AddRange(new object[] {
+            "Abonati",
+            "Moldovan Nicoleta",
+            "Marc Alexandru",
+            "Popovici Selina",
+            "Iurcut Anca"});
+            this.comboBox1_ListaNume.Location = new System.Drawing.Point(25, 388);
+            this.comboBox1_ListaNume.Name = "comboBox1_ListaNume";
+            this.comboBox1_ListaNume.Size = new System.Drawing.Size(236, 28);
+            this.comboBox1_ListaNume.TabIndex = 8;
+            // 
+            // comboBox2_Cartele
+            // 
+            this.comboBox2_Cartele.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2_Cartele.FormattingEnabled = true;
+            this.comboBox2_Cartele.Items.AddRange(new object[] {
+            "Cod Cartele ",
+            "SassyFit0001",
+            "SassyFit0002",
+            "SassyFit0003",
+            "SassyFit0004"});
+            this.comboBox2_Cartele.Location = new System.Drawing.Point(25, 459);
+            this.comboBox2_Cartele.Name = "comboBox2_Cartele";
+            this.comboBox2_Cartele.Size = new System.Drawing.Size(236, 28);
+            this.comboBox2_Cartele.TabIndex = 9;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(946, 272);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 535);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 26);
+            this.textBox1.TabIndex = 11;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(811, 54);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(86, 24);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Fitness";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(812, 114);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(81, 24);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Pilates";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(812, 84);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(84, 24);
+            this.radioButton3.TabIndex = 14;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Zumba";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(812, 144);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(88, 24);
+            this.radioButton4.TabIndex = 15;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Kickbox";
+            this.radioButton4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -116,6 +214,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1237, 591);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox2_Cartele);
+            this.Controls.Add(this.comboBox1_ListaNume);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -142,6 +248,14 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1_ListaNume;
+        private System.Windows.Forms.ComboBox comboBox2_Cartele;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
     }
 }
 
--- gymApp_03/Form1.cs
+++ gymApp_03/Form1.cs
@@ -11,9 +11,16 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        string [] abonati = { "Moldovan Nicoleta", "Marc Alexandru", "Popovici Selina", "Iurcut Anca" };
+        string [] codCartele ={ "SassyFit0001", "SassyFit0002", "SassyFit0003", "SassyFit0004" };
+        string [] imagini = { "mnicoleta.jpg", "malexandru.jpg", "pselina.jpg", "ianca.jpg" };
+        string[] inregAbonati = { "", "", "", "", "", "" };
+
         public Form1()
         {
             InitializeComponent();
+            dateTimePicker1.MinDate = DateTime.Now.AddDays(1);
+            comboBox_ListaNume.SelectedIndex = 0;
         }
     }
 }
